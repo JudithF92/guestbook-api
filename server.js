a8eb419ceacb725475fd6907483c4f5178a3ff45
Judith Feltl <judith.feltl@live.at>
2021-06-12 15:56:08 +0200

    connected server to heroku database

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const knex = require('knex');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host : '127.0.0.1',
-		user : 'postgres',
-		password : 'hashi',
-		database: 'guestbookDB'
+		connectionString : process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
